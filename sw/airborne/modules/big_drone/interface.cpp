197116d2b4ead5dc64d53a4a392debf8a034cb5f
maxvanbart <maxvanbart@gmail.com>
2023-03-15 15:25:23 +0100

    cleanup

--- sw/airborne/modules/big_drone/interface.cpp
+++ sw/airborne/modules/big_drone/interface.cpp
@@ -3,6 +3,6 @@
 #include "interface.h"
 
 int test_function(int n) {
-    std::cout << "This interface works!" << std::endl;
+    std::cout << "This interface works! " << n << std::endl;
     return n * n;
 }
